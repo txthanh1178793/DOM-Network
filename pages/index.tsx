bc6b63be247dbfe004bd7a1f18ebd6662439c503
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-20 21:54:43 +0700

    remove inter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Html } from 'next/document'
 import Head from 'next/head'
-
-const inter = Inter({ subsets: ['latin'] })
-
 
 export default function Home() {
   return (
